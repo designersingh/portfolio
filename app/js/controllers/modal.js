01b0d9eec98f6c67b7671ae3be047b22531db7d5
designersingh <tajinderpalsohi@gmail.com>
2016-07-23 20:53:20 -0700

    added Keyboard support for the Modal-carousel navigation on about page.

--- app/js/controllers/modal.js
+++ app/js/controllers/modal.js
@@ -4,4 +4,16 @@ dApp.controller('modalCtrl',['$scope','$rootScope','$http','ngDialog', function(
     $scope.images = $scope.$parent.instaImages;
     $scope.currentIndex = $scope.$parent.index;
 
+
+    $scope.carouselNav = function(event){
+        if(event.keyCode==39){
+            $('#myCarousel').carousel('next');
+        }
+
+        else if(event.keyCode==37){
+            $('#myCarousel').carousel('prev');
+        }
+    }
+
+
 }]);
